ced25d8dfa4dda9da7d85f6d4667753e121c8852
Yuvaraju Sai Kumar <157206085+yuvarajusaikumar@users.noreply.github.com>
2024-12-07 16:44:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# Create an S3 bucket for file storage
+# Create an S3 bucket for file storage 
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "example-bucket-12345"
   acl    = "private"
